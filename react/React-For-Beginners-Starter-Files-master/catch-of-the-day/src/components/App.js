949ef273518fc771feacee93e0845a736e109bd0
Sweet4ever <oskpil@gmail.com>
2022-02-07 14:24:19 +0100

    vid16

--- react/React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/App.js
+++ react/React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/App.js
@@ -18,6 +18,11 @@ class App extends React.Component{
     loadSampleFishes = () => {
         this.setState({fishes: sampleFishes});
     };
+    addToOrder = (key) => {
+        const order = {...this.state.order};
+        order[key] = order[key] +1 || 1;
+        this.setState({order});
+    }
     render(){
         return(
             <div className="catch-of-the-day">
@@ -25,7 +30,7 @@ class App extends React.Component{
                     <Header tagline="Fresh Seafood Market" />
                     <ul className="fishes">
                         {Object.keys(this.state.fishes).map(key => 
-                            <Fish key={key} details={this.state.fishes[key]}></Fish>
+                            <Fish key={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} index={key}></Fish>
                         )}
                     </ul>
                 </div>
