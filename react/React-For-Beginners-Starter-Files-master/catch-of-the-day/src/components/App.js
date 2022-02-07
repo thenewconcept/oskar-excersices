eac6fafef34aeee22e1291b68f09f1cb18a1d802
Sweet4ever <oskpil@gmail.com>
2022-02-07 13:55:10 +0100

    vid14

--- react/React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/App.js
+++ react/React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
+import sampleFishes from "../sample-fishes"
 
 class App extends React.Component{
     state = {
@@ -13,6 +14,9 @@ class App extends React.Component{
         fishes[`fish${Date.now()}`] = fish;
         this.setState({fishes});
     };
+    loadSampleFishes = () => {
+        this.setState({fishes: sampleFishes});
+    };
     render(){
         return(
             <div className="catch-of-the-day">
@@ -20,7 +24,7 @@ class App extends React.Component{
                     <Header tagline="Fresh Seafood Market" />
                 </div>
                 <Order></Order>
-                <Inventory addFish={this.addFish} />
+                <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
             </div>
         );
     }
